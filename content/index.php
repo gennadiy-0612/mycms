0d85dc30c9e7b7beb64594b8013fed164231b7a8
Genn <intval@i.ua>
2021-04-26 17:14:07 +0300

    start

--- content/index.php
+++ content/index.php
@@ -3,12 +3,11 @@ ob_start();
 $folders = array_map(function ($dir) {
     return basename($dir);
 }, glob('../content/*', GLOB_ONLYDIR));
-echo json_encode($folders);
 $counter = count($folders);
 $counted = '';
 for ($i = 0; $i < $counter; $i++) {
-    $counted = $folders[$i];
+    $counted .= '<li> ' . $folders[$i] . '</li>';
 }
-$out = ob_get_contents();
+$ol = '<ol>' . $counted . '</ol>';
 ob_end_clean();
-echo $out;
+echo $ol;
