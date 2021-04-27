bc7638b8084f1a5bcedfd5c21d6e9f302dcee4bc
Genn <intval@i.ua>
2021-04-27 16:06:39 +0300

    start

--- content/Photo/ex.php
+++ content/Photo/ex.php
@@ -1,12 +1,24 @@
 <?php
 echo "test1.jpg:<br />\n";
 $exif = exif_read_data('IMG_20200308_115201_EDIT_1-01.jpeg');
-echo $exif===false ? "Не найдено данных заголовка.<br />\n" : "Изображение содержит заголовки<br />\n";
+$fp = fopen('IMG_20200308_115201_EDIT_1-01.jpeg', 'rb');
 
-$exif = exif_read_data('IMG_20200308_115201_EDIT_1-01.jpeg', 0, true);
-echo "test2.jpg:<br />\n";
-foreach ($exif as $key => $section) {
-    foreach ($section as $name => $val) {
-        echo "$key.$name: $val<br />\n";
-    }
+if (!$fp) {
+    echo 'Ошибка: Невозможно открыть файл для чтения';
+    exit;
+}
+
+// Попытка прочитать заголовки exif
+$headers = exif_read_data($fp);
+
+if (!$headers) {
+    echo 'Ошибка: невозможно прочитать заголовки exif';
+    exit;
+}
+
+// Напечатать заголовки 'COMPUTED'
+echo 'Заголовки EXIF:' . PHP_EOL;
+
+foreach ($headers['COMPUTED'] as $header => $value) {
+    printf(' %s => %s%s', $header, $value, PHP_EOL);
 }
