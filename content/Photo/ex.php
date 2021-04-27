93868642433743e3d1ca58ed727c6740b8b42d58
Genn <intval@i.ua>
2021-04-27 17:22:06 +0300

    start

--- content/Photo/ex.php
+++ content/Photo/ex.php
@@ -1,13 +1,15 @@
 <?php
 ob_start("ob_gzhandler");
-echo "test1.jpg:<br />\n";
-$exif = exif_read_data('IMG_20200308_115201_EDIT_1-01.jpeg');
+$src = 'IMG_20200308_115201_EDIT_1-01.jpeg';
+echo '<img src="',$src,'">';
+$exif = exif_read_data($src);
 echo $exif===false ? "Не найдено данных заголовка.<br />\n" : "Изображение содержит заголовки<br />\n";
 
-$exif = exif_read_data('IMG_20200308_115201_EDIT_1-01.jpeg', 0, true);
+$exif = exif_read_data($src, 0, true);
 $exifAll = '';
 foreach ($exif as $key => $section) {
     foreach ($section as $name => $val) {
+//        '<pre>'.var_dump($key.$name).'</pre><br>';
         $exifAll = $exifAll. "<p><strong>$key.$name</strong>: $val</p>";
     }
 }
