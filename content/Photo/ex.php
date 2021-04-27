57cbae8f768a624830c52f7fa737bec24f1af859
Genn <intval@i.ua>
2021-04-27 17:51:03 +0300

    start

--- content/Photo/ex.php
+++ content/Photo/ex.php
@@ -1,16 +1,17 @@
 <?php
 ob_start("ob_gzhandler");
 $src = 'IMG_20200308_115201_EDIT_1-01.jpeg';
-echo '<img src="',$src,'">';
+echo '<img src="', $src, '">';
 $exif = exif_read_data($src);
-echo $exif===false ? "Не найдено данных заголовка.<br />\n" : "Изображение содержит заголовки<br />\n";
-
 $exif = exif_read_data($src, 0, true);
 $exifAll = '';
 foreach ($exif as $key => $section) {
     foreach ($section as $name => $val) {
-//        '<pre>'.var_dump($key.$name).'</pre><br>';
-        $exifAll = $exifAll. "<p><strong>$key.$name</strong>: $val</p>";
+        if (is_array($val)) {
+            $exifAll = $exifAll . '<p><strong>' . $key . $name . '</strong>:' . $val[0] . '</p>';
+            $exifAll = $exifAll . '<p><strong>' . $key . $name . '</strong>:' . $val[1] . '</p>';
+            $exifAll = $exifAll . '<p><strong>' . $key . $name . '</strong>:' . $val[2] . '</p>';
+        } else $exifAll = $exifAll . "<p><strong>$key.$name</strong>: $val</p>";
     }
 }
 echo $exifAll;
