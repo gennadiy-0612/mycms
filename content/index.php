fb1f924f9f980343d26698794c4359f3351fd597
Genn <intval@i.ua>
2021-04-27 14:25:13 +0300

    start

--- content/index.php
+++ content/index.php
@@ -7,7 +7,7 @@ $counted = '';
 $fold = '';
 for ($i = 0; $i < $counter; $i++) {
     $fold = explode('/', $folders[$i]);
-    $counted .= '<li><a href="' . $_SERVER['SERVER_NAME'] . '/' . $fold[1] . '/' . $fold[2] . '/list.php">' . $fold[2] . '</a></li>';
+    $counted .= '<li><a href="' . $_SERVER['SERVER_NAME'] . '/' . $fold[1]  . $fold[2] . '/list.php">' . $fold[2] . '</a></li>';
     echo  $_SERVER['SERVER_NAME'] . '/' . $fold[1] . '/' . $fold[2] . '/list.php"<br>';
 }
 //ob_end_clean();
