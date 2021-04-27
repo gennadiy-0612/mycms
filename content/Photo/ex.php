fc654de8a56197022dec7f2e84f50ef5e5131576
Genn <intval@i.ua>
2021-04-27 15:57:45 +0300

    start

--- content/Photo/ex.php
+++ content/Photo/ex.php
@@ -1,23 +1,13 @@
 <?php
-// Открываем файл в бинарном режиме
-$fp = fopen('IMG_20200308_115201_EDIT_1-01.jpeg', 'rb');
+echo "test1.jpg:<br />\n";
+$exif = exif_read_data('IMG_20200308_115201_EDIT_1-01.jpeg');
+echo $exif===false ? "Не найдено данных заголовка.<br />\n" : "Изображение содержит заголовки<br />\n";
 
-if (!$fp) {
-    echo 'Ошибка: Невозможно открыть файл для чтения';
-    exit;
+$exif = exif_read_data('tests/test2.jpg', 0, true);
+echo "test2.jpg:<br />\n";
+foreach ($exif as $key => $section) {
+    foreach ($section as $name => $val) {
+        echo "$key.$name: $val<br />\n";
+    }
 }
-
-// Попытка прочитать заголовки exif
-$headers = exif_read_data($fp);
-
-if (!$headers) {
-    echo 'Ошибка: невозможно прочитать заголовки exif';
-    exit;
-}
-
-// Напечатать заголовки 'COMPUTED'
-echo 'Заголовки EXIF:' . PHP_EOL;
-
-foreach ($headers['COMPUTED'] as $header => $value) {
-    printf(' %s => %s%s', $header, $value, PHP_EOL);
-}
+?>
