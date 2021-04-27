baa51f50de3950d2b8bb4c2bfa7e6fe27e1622bc
Genn <intval@i.ua>
2021-04-27 15:54:40 +0300

    start

---
+++ content/Photo/ex.php
@@ -0,0 +1,23 @@
+<?php
+// Открываем файл в бинарном режиме
+$fp = fopen('IMG_20200308_115201_EDIT_1-01.jpeg', 'rb');
+
+if (!$fp) {
+    echo 'Ошибка: Невозможно открыть файл для чтения';
+    exit;
+}
+
+// Попытка прочитать заголовки exif
+$headers = exif_read_data($fp);
+
+if (!$headers) {
+    echo 'Ошибка: невозможно прочитать заголовки exif';
+    exit;
+}
+
+// Напечатать заголовки 'COMPUTED'
+echo 'Заголовки EXIF:' . PHP_EOL;
+
+foreach ($headers['COMPUTED'] as $header => $value) {
+    printf(' %s => %s%s', $header, $value, PHP_EOL);
+}
