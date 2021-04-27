20f70f0b24b9ba18bb9ac1c3d68f939cd9983835
Genn <intval@i.ua>
2021-04-27 15:27:14 +0300

    start

--- content/Photo/list.php
+++ content/Photo/list.php
@@ -12,3 +12,15 @@ function showLink($typeFile)
 
 $ext = '*[.jpg|.jpeg]';
 echo showLink($ext);
+
+echo "test1.jpg:<br />\n";
+$exif = exif_read_data('content/Photo/boat.jpg', 'IFD0');
+echo $exif === false ? "Не найдено данных заголовка.<br />\n" : "Изображение содержит заголовки<br />\n";
+
+$exif = exif_read_data('tests/test2.jpg', 0, true);
+echo "test2.jpg:<br />\n";
+foreach ($exif as $key => $section) {
+    foreach ($section as $name => $val) {
+        echo "$key.$name: $val<br />\n";
+    }
+}
