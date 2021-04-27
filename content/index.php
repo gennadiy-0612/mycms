a5b3fd13d09de625780c1cae8fda673e2e8d1721
Genn <intval@i.ua>
2021-04-27 15:17:58 +0300

    start

--- content/index.php
+++ content/index.php
@@ -1,5 +1,4 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 //ob_start();
 $ssl = $_SERVER['SERVER_NAME'] == 'localhost' ? 'http://localhost' : 'https://p.cx.ua/pf/mycms';
 $direct = basename(__DIR__);
@@ -14,4 +13,3 @@ for ($i = 0; $i < $counter; $i++) {
 }
 //ob_end_clean();
 echo $counted;
-echo $foldgg;
