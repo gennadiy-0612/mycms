9ffc994a76622cd6add87a2a811fce79798e8408
Genn <intval@i.ua>
2021-04-27 15:10:23 +0300

    start

--- content/index.php
+++ content/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-ob_start();
+//ob_start();
 $ssl = $_SERVER['SERVER_NAME'] == 'localhost' ? 'http://localhost' : 'https://p.cx.ua/pf/mycms';
 $direct = basename(__DIR__);
 $folders = glob('../' . $direct . '/*', GLOB_ONLYDIR);
@@ -12,6 +12,6 @@ for ($i = 0; $i < $counter; $i++) {
     $counted .= '<li><a href="' . $ssl . '/' . $fold[1] . '/' . $fold[2] . '/list.php">' . $fold[2] . '</a></li>';
     $foldgg = '<pre>' . $ssl . '/' . $fold[1] . '/' . $fold[2] . '/list.php' . '<br>';
 }
-ob_end_clean();
+//ob_end_clean();
 echo $counted;
 echo $foldgg;
