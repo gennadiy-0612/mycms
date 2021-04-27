e60dd8764bbb991b0e2fc3c836f6689b4a5d8445
Genn <intval@i.ua>
2021-04-27 16:11:07 +0300

    start

--- content/Photo/ex.php
+++ content/Photo/ex.php
@@ -7,6 +7,6 @@ $exif = exif_read_data('IMG_20200308_115201_EDIT_1-01.jpeg', 0, true);
 echo "test2.jpg:<br />\n";
 foreach ($exif as $key => $section) {
     foreach ($section as $name => $val) {
-        echo "<p>$key.$name: <strong>$val</strong></p>";
+        echo "<p><strong>$key.$name<:/strong> $val</p>";
     }
 }
