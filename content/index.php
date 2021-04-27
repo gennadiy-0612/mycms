cba196d48818646db8b4dd6251c927b06f1a9f59
Genn <intval@i.ua>
2021-04-27 14:57:10 +0300

    start

--- content/index.php
+++ content/index.php
@@ -3,6 +3,7 @@ ob_start();
 $ssl = $_SERVER['SERVER_NAME'] == 'localhost' ? 'http://' : 'https://';
 $slash = $_SERVER['SERVER_NAME'] == 'localhost' ? '/' : '';
 $direct = explode("\\", __DIR__)[4];
+echo $direct;
 $folders = glob('../' . $direct . '/*', GLOB_ONLYDIR);
 $counter = count($folders);
 $counted = '';
@@ -10,8 +11,7 @@ $fold = '';
 for ($i = 0; $i < $counter; $i++) {
     $fold = explode('/', $folders[$i]);
     $counted .= '<li><a href="' . $ssl . $_SERVER['SERVER_NAME'] . $fold[1] . '/' . $fold[2] . '/list.php">' . $fold[2] . '</a></li>';
-    echo '<pre>', $ssl . $_SERVER['SERVER_NAME']  . $fold[1] . '/' . $fold[2] . '/list.php', '<br>';
+    echo '<pre>', $ssl . $_SERVER['SERVER_NAME'] . $fold[1] . '/' . $fold[2] . '/list.php', '<br>';
 }
 //ob_end_clean();
 echo $counted;
-echo basename(__DIR__);
