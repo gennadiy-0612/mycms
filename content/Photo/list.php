138dd4ef3dde6bde559494689d1f076f4dac8e00
Genn <intval@i.ua>
2021-04-27 18:52:40 +0300

    start

--- content/Photo/list.php
+++ content/Photo/list.php
@@ -4,7 +4,6 @@ function showLink($typeFile)
 {
     $links = '';
     foreach (glob($typeFile) as $filename) {
-        echo $filename,'<br>';
         $links = $links . '<li><a href="' . $filename . '">' . $filename . '</a>';
     }
     return $links = '<ol>' . $links . '</ol>';
@@ -12,26 +11,3 @@ function showLink($typeFile)
 
 $ext = '*[.jpg|.jpeg]';
 echo showLink($ext);
-
-// Открываем файл в бинарном режиме
-$fp = fopen('IMG_20200308_115201_EDIT_1-01.jpeg', 'rb');
-
-if (!$fp) {
-    echo 'Ошибка: Невозможно открыть файл для чтения';
-    exit;
-}
-
-// Попытка прочитать заголовки exif
-$headers = exif_read_data($fp);
-
-if (!$headers) {
-    echo 'Ошибка: невозможно прочитать заголовки exif';
-    exit;
-}
-
-// Напечатать заголовки 'COMPUTED'
-echo 'Заголовки EXIF:' . PHP_EOL;
-
-foreach ($headers['COMPUTED'] as $header => $value) {
-    printf(' %s => %s%s', $header, $value, PHP_EOL);
-}
