6b0b9b64b291d508a175120233f832179d390de2
Genn <intval@i.ua>
2021-04-27 15:00:55 +0300

    start

--- content/index.php
+++ content/index.php
@@ -2,15 +2,15 @@
 ob_start();
 $ssl = $_SERVER['SERVER_NAME'] == 'localhost' ? 'http://' : 'https://';
 $slash = $_SERVER['SERVER_NAME'] == 'localhost' ? '/' : '';
-$direct = explode("\\", __DIR__)[4];
+$direct = basename(__DIR__);
 $folders = glob('../' . $direct . '/*', GLOB_ONLYDIR);
 $counter = count($folders);
 $counted = '';
 $fold = '';
 for ($i = 0; $i < $counter; $i++) {
     $fold = explode('/', $folders[$i]);
-    $counted .= '<li><a href="' . $ssl . $_SERVER['SERVER_NAME'] . $fold[1] . '/' . $fold[2] . '/list.php">' . $fold[2] . '</a></li>';
-    echo '<pre>', $ssl . $_SERVER['SERVER_NAME']  . $fold[1] . '/' . $fold[2] . '/list.php', '<br>';
+    $counted .= '<li><a href="' . $ssl . $_SERVER['SERVER_NAME']. $slash . $fold[1] . $slash . $fold[2] . '/list.php">' . $fold[2] . '</a></li>';
+    echo '<pre>', $ssl . $_SERVER['SERVER_NAME'] . $slash . $fold[1] . $slash . $fold[2] . '/list.php', '<br>';
 }
 //ob_end_clean();
 echo $counted;
