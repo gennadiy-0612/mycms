bda504b35d36eec51a2187e8c908fb41fd49e114
Genn <intval@i.ua>
2021-04-27 15:58:13 +0300

    start

--- content/Photo/ex.php
+++ content/Photo/ex.php
@@ -3,11 +3,10 @@ echo "test1.jpg:<br />\n";
 $exif = exif_read_data('IMG_20200308_115201_EDIT_1-01.jpeg');
 echo $exif===false ? "Не найдено данных заголовка.<br />\n" : "Изображение содержит заголовки<br />\n";
 
-$exif = exif_read_data('tests/test2.jpg', 0, true);
+$exif = exif_read_data('IMG_20200308_115201_EDIT_1-01.jpeg', 0, true);
 echo "test2.jpg:<br />\n";
 foreach ($exif as $key => $section) {
     foreach ($section as $name => $val) {
         echo "$key.$name: $val<br />\n";
     }
 }
-?>
