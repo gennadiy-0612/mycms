816eba5d606e13fbb00acc0fe6fb4441c19a208c
Genn <intval@i.ua>
2021-04-29 09:16:28 +0300

    start

--- content/Photo/list.php
+++ content/Photo/list.php
@@ -4,10 +4,11 @@ function showLink($typeFile): string
 {
     $links = '';
     foreach (glob($typeFile, GLOB_BRACE) as $filename) {
-        $links = $links . '<li><a href="water.php?php-id=' . $filename . '"><strong>' . $filename . '</strong><br><img width="100" src="water.php?php-id=' . $filename .'"</a>';
+        $links = $links . '<li><a href="water.php?php-id=' . $filename . '"><strong>' . $filename . '</strong></a>';
     } 
     return $links = '<ol>' . $links . '</ol>';
 }
 
 $ext = "*.{[jJ][pP]{,[eE]}[gG],[tT][iI][fF]{,[fF]}}";
+echo '<a href="/">Home</a>';
 echo showLink($ext);
