aa3a674793e8ff52b03d3a8df07bcf989b54babb
Genn <intval@i.ua>
2021-04-29 10:12:38 +0300

    start

--- content/HTML/list.php
+++ content/HTML/list.php
@@ -4,7 +4,7 @@ ob_start("ob_gzhandler");
 function showLink($typeFile)
 {
     $links = '';
-    foreach (glob(utf8_encode($typeFile)) as $filename) {
+    foreach (glob(utf8_decode($typeFile)) as $filename) {
         $links = $links . '<li><a href="' . $filename . '">' . $filename . '</a>';
     }
     return $links = '<ol>' . $links . '</ol>';
